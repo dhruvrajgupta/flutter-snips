fd51e02f01dd5be1e92c680c0f887c75e36dc265
dhruvrajgupta <dhruv.learner@gmail.com>
2020-10-16 00:07:32 +0530

    flash-chat with ColorTween Animation

--- flash-chat/screens/welcome_screen.dart
+++ flash-chat/screens/welcome_screen.dart
@@ -18,17 +18,10 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   void initState() {
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 1));
-    animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
 
     controller.forward();
-
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
 
     controller.addListener(() {
       setState(() {});
@@ -45,7 +38,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -58,7 +51,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 160.0 * animation.value,
+                    height: 60.0,
                   ),
                 ),
                 Text(
